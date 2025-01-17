62c4bcabe21805b3aa6472090d7b03e05bfeb10a
Chubbi-Stephen <udeokoliestephen@gmail.com>
2025-01-17 02:09:40 +0100

    switched to modules

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,4 @@
-// const products = [
-// 	{
-// 		image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-// 		name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-// 		rating: {
-// 			stars: 4.5,
-// 			count: 87,
-// 		},
-// 		priceCents: 1090,
-// 	},
-// 	{
-// 		image: "images/products/intermediate-composite-basketball.jpg",
-// 		name: "Intermediate Size Basketball",
-// 		rating: {
-// 			stars: 4.0,
-// 			count: 127,
-// 		},
-// 		priceCents: 2095,
-// 	},
-// 	{
-// 		image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-// 		name: "Adults Plain Cotton T-Shirt - 2 Pack",
-// 		rating: {
-// 			stars: 4.5,
-// 			count: 56,
-// 		},
-// 		priceCents: 799,
-// 	},
-// ];
+import { cart } from "../data/cart.js";
 
 let productHTML = "";
 
@@ -121,7 +93,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
 			Number(quantitySelector);
 
 		// Timer for the popup message
-		// setTimeout(addedTimeout, 0);
+		setTimeout(addedTimeout, 0);
 
 		function addedTimeout() {
 			document
@@ -153,8 +125,6 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
 		document.querySelectorAll(".js-add-to-cart").forEach((button) => {
 			button.addEventListener("click", refreshTimeout);
 		});
-
-		// check out the popup imbalance
 
 		console.log(cart);
 	});
